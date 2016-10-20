8e726e60835bbb9a51170cdb3a1c728e424e64e6
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-20 19:48:53 +0800

    updated join controller

--- src/callbacks/join.js
+++ src/callbacks/join.js
@@ -2,11 +2,47 @@ const bot = require('../bot');
 const models = require('../database');
 const Promise = require('bluebird');
 
-module.exports = (callbackId, userId, username, code, num) => {
+let createTicket = (numberOfPeople, _user, _event) => {
+  return models.sequelize
+    .transaction((t) => {
+      var promises = [];
+      promises.push(
+        models.Ticket
+          .create(
+            {
+              "userId": _user.userId,
+              "eventId": _event.eventId,
+              "noOfPeople": num
+            },
+            { transaction: t }
+          )
+      );
+
+      promises.push(
+        _user
+          .update(
+            {
+              "isInQueue": true
+            },
+            { transaction: t }
+          )
+      );
+
+      return Promise.all(promises);
+    });
+};
+
+module.exports = (query, code, num) => {
+  let name = query.message.chat.first_name + " " + query.message.chat.last_name;
+  let userId = query.message.chat.id;
+  let callbackId = query.id;
+
   let _user = null;
   let _event = null;
+  console.log(query.message)
+  bot.editMessageReplyMarkup({reply_markup: {inline_keyboard: [[]]}}, {chat_id: userId, message_id: query.message.message_id});
   return models.User
-    .findOrCreate({ where: { name: username, userId: userId } })
+    .findOrCreate({ where: { name: name, userId: userId } })
     .then((users) => {
       _user = users[0];
       if (_user.isInQueue) {
@@ -20,35 +56,12 @@ module.exports = (callbackId, userId, username, code, num) => {
         throw new Error('The event code entered was not found.');
       }
       _event = event;
-      return models.sequelize
-        .transaction((t) => {
-          var promises = [];
-          promises.push(
-            models.Ticket
-              .create(
-                {
-                  "userId": _user.userId,
-                  "eventId": _event.eventId,
-                  "noOfPeople": num
-                },
-                { transaction: t }
-              )
-          );
-          promises.push(
-            _user
-              .update(
-                {
-                  "isInQueue": true
-                },
-                { transaction: t }
-              )
-          );
-          return Promise.all(promises);
-        });
+      return createTicket(num, _user, _event);
     })
-    .then(() => {
-      bot.answerCallbackQuery(callbackId, '', true);
-      bot.sendMessage(userId, 'You have joined the queue for ' + _event.eventName + ' with ' + num + ' people.\n\nI will notify you when it is 5 minutes before your turn. Show up promptly or I will give your turn to someone else \u{1F608}.');
+    .then((result) => {
+      let ticket = result[0];
+      bot.answerCallbackQuery(callbackId, 'You have joined the queue for ' + _event.eventName + '.', true);
+      bot.sendMessage(userId, 'Ticket #' + ticket.ticketId + '\n\nYou have joined the queue for ' + _event.eventName + ' with ' + num + ' people.\n\nI will notify you when it is 5 minutes before your turn. Show up promptly or I will give your turn to someone else \u{1F608}.', {hide_keyboard: true});
     })
     .catch((err) => {
       bot.sendMessage(userId, err.message);
