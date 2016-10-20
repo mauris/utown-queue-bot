af598a88e4c619775059351503c48f564227c6d0
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-20 15:48:18 +0800

    implemented join command

---
+++ src/commands/join.js
@@ -0,0 +1,50 @@
+const bot = require('../bot');
+const models = require('../database');
+
+const BOT_USERNAME = process.env.BOT_USERNAME;
+const COMMAND_REGEX = /^\/join(@\w+)*(\s+(.+))*\s*/i;
+
+bot.onText(COMMAND_REGEX, (msg, match) => {
+  var replyChatId = msg.chat.id;
+  if (msg.chat.type !== 'private' || (match[1] && match[1].toLowerCase() !== BOT_USERNAME)) {
+    return;
+  }
+
+  var eventCode = match[3];
+  bot
+    .sendMessage(replyChatId, "How many people is joining together (including yourself)?", {
+      reply_markup: {
+          inline_keyboard: [[
+            {
+              text: "1",
+              callback_data: JSON.stringify(["join", eventCode, "1"]),
+            },
+            {
+              text: "2",
+              callback_data: JSON.stringify(["join", eventCode, "2"]),
+            },
+            {
+              text: "3",
+              callback_data: JSON.stringify(["join", eventCode, "3"]),
+            },
+            {
+              text: "4",
+              callback_data: JSON.stringify(["join", eventCode, "4"]),
+            },
+            {
+              text: "5",
+              callback_data: JSON.stringify(["join", eventCode, "6"]),
+            },
+            {
+              text: "6",
+              callback_data: JSON.stringify(["join", eventCode, "6"]),
+            }
+          ]],
+      },
+    })
+    .catch(console.error);
+
+  /*
+  var eventCode = match[3];
+  */
+});
