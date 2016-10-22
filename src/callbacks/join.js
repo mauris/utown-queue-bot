5d7ea14d013b11b040b8baf015a946b734a80dc4
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-22 23:12:33 +0800

    fixed join callback to use transaction
    so that concurrent calls do not cause duplicate ticket creation for a
    single user

--- src/callbacks/join.js
+++ src/callbacks/join.js
@@ -42,27 +42,30 @@ module.exports = (query, code, num) => {
 
   bot.sendChatAction(userId, "typing");
   bot.editMessageReplyMarkup({reply_markup: {inline_keyboard: [[]]}}, {chat_id: userId, message_id: query.message.message_id});
-  return models.User
-    .findOrCreate({ where: { name: name, userId: userId } })
-    .then((users) => {
-      _user = users[0];
-      if (_user.isInQueue) {
-        throw new Error('You are already queueing up for an activity.');
-      }
-      return models.Event
-        .find({ where: { eventCode: code } });
-    })
-    .then((event) => {
-      if (!event) {
-        throw new Error('The event code entered was not found.');
-      }
-      _event = event;
-      return createTicket(num, _user, _event);
-    })
-    .then((result) => {
-      let ticket = result[0];
-      bot.answerCallbackQuery(callbackId, 'You have joined the queue for ' + _event.eventName + '.', true);
-      bot.sendMessage(userId, 'Ticket #' + ticket.ticketId + '\n\nYou have joined the queue for ' + _event.eventName + ' with ' + num + ' people.\n\nI will notify you when it is 5 minutes before your turn. Show up promptly or I will give your turn to someone else \u{1F608}.', {hide_keyboard: true});
+  return models.sequelize
+    .transaction((t) => {
+      return models.User
+        .findOrCreate({ where: { name: name, userId: userId } })
+        .then((users) => {
+          _user = users[0];
+          if (_user.isInQueue) {
+            throw new Error('You are already queueing up for an activity.');
+          }
+          return models.Event
+            .find({ where: { eventCode: code } });
+        })
+        .then((event) => {
+          if (!event) {
+            throw new Error('The event code entered was not found.');
+          }
+          _event = event;
+          return createTicket(num, _user, _event);
+        })
+        .then((result) => {
+          let ticket = result[0];
+          bot.answerCallbackQuery(callbackId, 'You have joined the queue for ' + _event.eventName + '.', true);
+          bot.sendMessage(userId, 'Ticket #' + ticket.ticketId + '\n\nYou have joined the queue for ' + _event.eventName + ' with ' + num + ' people.\n\nI will notify you when it is 5 minutes before your turn. Show up promptly or I will give your turn to someone else \u{1F608}.', {hide_keyboard: true});
+        })
     })
     .catch((err) => {
       bot.sendMessage(userId, err.message);
