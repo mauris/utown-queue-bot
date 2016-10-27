2e14a282a243e6717dece61aafdfd2f8f63d9840
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-27 12:47:27 +0800

    updated bot to send posters in /map command

--- src/commands/map.js
+++ src/commands/map.js
@@ -9,5 +9,7 @@ bot.onText(COMMAND_REGEX, (msg, match) => {
   if (msg.chat.type !== 'private' || (match[1] && match[1].toLowerCase() !== BOT_USERNAME)) {
     return;
   }
-  return bot.sendMessage(replyChatId, 'This feature is currently not available yet. But we potentially can send a picture of the UTown map so that people know where to go.');
+  bot.sendChatAction(replyChatId, 'upload_photo');
+  bot.sendPhoto(replyChatId, "https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-9/14691139_10210466760522686_7236882079378429634_n.jpg?oh=a6f834653bcd2343704417f00d410419&oe=58A59525");
+  bot.sendPhoto(replyChatId, "https://scontent-sit4-1.xx.fbcdn.net/v/t1.0-0/s480x480/14702359_10205731133598106_6823097246719895381_n.jpg?oh=75c1457eaf2278d0323369abed996b34&oe=58A01DC3", {caption: "There's also an amazing line up of performances at Basement of Residential College 4."});
 });
