d1ae18382363ea115b032194a320c361b351f988
Sam-Mauris Yong <mauris@hotmail.sg>
2016-10-27 12:52:49 +0800

    updated college names to be alphabetical order

--- src/commands/about.js
+++ src/commands/about.js
@@ -9,5 +9,5 @@ bot.onText(COMMAND_REGEX, (msg, match) => {
   if (msg.chat.type !== 'private' || (match[1] && match[1].toLowerCase() !== BOT_USERNAME)) {
     return;
   }
-  return bot.sendMessage(replyChatId, '*UTown Pandemonium: Halloween Haunted Houses*\n\nBrought to you by:\n- Tembusu College\n- Residential College 4\n- College of Alice and Peter Tan\n- Cinnamon College\n- Yale-NUS College\n\nToyol was conceived by ZeQi and written by Sam. You can find my remains at https://github.com/mauris/utown-queue-bot', { parse_mode: 'Markdown', disable_web_page_preview: true });
+  return bot.sendMessage(replyChatId, '*UTown Pandemonium: Halloween Haunted Houses*\n\nBrought to you by:\n- Cinnamon College\n- College of Alice and Peter Tan\n- Residential College 4\n- Tembusu College\n- Yale-NUS College\n\nToyol was conceived by ZeQi and written by Sam. You can find my remains at https://github.com/mauris/utown-queue-bot', { parse_mode: 'Markdown', disable_web_page_preview: true });
 });
